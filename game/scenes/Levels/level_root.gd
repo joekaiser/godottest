d19609e9fef4aebe6616f355af5d6511b94061b4
Joe Kaiser <joe.kaiser@slalom.com>
2017-05-11 14:19:39 -0700

    added the start of a gui

--- game/scenes/Levels/level_root.gd
+++ game/scenes/Levels/level_root.gd
@@ -1,8 +1,14 @@
 extends Node
 
 export var score = 0;
+var last_score=0;
+var score_text
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	set_fixed_process(true)
+	score_text=get_node("gui").get_node("lbl_score")
+	
+func _fixed_process(delta):
+	if last_score != score:
+		score_text.set_text(str(score))
+		last_score = score;
