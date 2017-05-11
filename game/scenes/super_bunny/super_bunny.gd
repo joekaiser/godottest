43f7359aadefdb4692ddf0d50e0f08cc79a465fb
Joe Kaiser <joe.kaiser@slalom.com>
2017-05-11 13:51:57 -0700

    added moving platforms

--- game/scenes/super_bunny/super_bunny.gd
+++ game/scenes/super_bunny/super_bunny.gd
@@ -22,7 +22,6 @@ var airborn_time=0
 
 func _ready():
 	set_process_input(true)
-	set_process(true)
 	set_fixed_process(true)
 	sprite = get_node("AnimatedSprite")
 	sprite.play("idle")
@@ -35,10 +34,10 @@ func _input(event):
 	if event.is_action_pressed("ui_accept") and jump_count < MAX_JUMPS:
 		jump()
 
-func _process(delta):
+func _fixed_process(delta):
 	var move_left = Input.is_action_pressed("ui_left")
 	var move_right = Input.is_action_pressed("ui_right")
-
+	var floor_velocity = Vector2()
 	var found_floor=false
 	velocity.y += delta * GRAVITY
 
@@ -64,20 +63,37 @@ func _process(delta):
 		var n = get_collision_normal()
 		motion = n.slide(motion)
 		velocity = n.slide(velocity)
-		move(motion)
-		
+
 		if(n == Vector2(0,-1)):
 			found_floor = true
+			floor_velocity = get_collider_velocity()
 			jump_count = 0
+			
+		if floor_velocity != Vector2():
+			motion += floor_velocity * delta
+
+		move(motion)
 	
 	if found_floor:
 		airborn_time = 0
 	else:
 		airborn_time += delta;
 		
-	if airborn_time > .1: ##not falling
+	if airborn_time > .1: 
 		play_animation("falling")
 		
+	
+	constrain_pos()
+
+
+
+func jump():
+	velocity.y=-JUMP_FORCE
+	jump_count += 1
+	var i = randi()%3+1
+	sfx.play("jump"+str(i))
+	
+func constrain_pos():
 	var pos = get_pos()
 	if pos.x < limit_left:
 		pos.x = limit_left
@@ -91,11 +107,3 @@ func _process(delta):
 	if pos.y > limit_down:
 		pos.y = limit_down
 		set_pos(pos)
-	
-
-
-func jump():
-	velocity.y=-JUMP_FORCE
-	jump_count += 1
-	var i = randi()%3+1
-	sfx.play("jump"+str(i))
